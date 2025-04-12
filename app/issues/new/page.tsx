326ad22b77c29f485a6efd7ffc59b24bd9076898
Priyanshu <priyanshulamkhade@gmail.com>
2025-04-12 18:22:56 +0530

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,7 +7,8 @@ const newIssuePage = () => {
     <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder="Issue"></TextField.Root>
       <TextArea placeholder="Description" /> 
-      <button className='bg-blue-600 px-2.5 py-1.5 font-medium text-sm text-white rounded-sm cursor-pointer'>Submit New Issue</button>
+      <button 
+      className='bg-violet-500 px-2.5 py-1.5 font-medium text-sm text-white rounded-lg cursor-pointer'>Submit New Issue</button>
     </div>
   )
 }
