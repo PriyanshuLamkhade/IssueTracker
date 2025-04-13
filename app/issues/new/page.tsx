2b78ffa2f12587d1c550f3c17b40227587a9c317
Priyanshu <priyanshulamkhade@gmail.com>
2025-04-13 12:00:47 +0530

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Callout, TextField } from '@radix-ui/themes'
+import { Callout, Text, TextField } from '@radix-ui/themes'
 import dynamic from 'next/dynamic'
 import React, { useState } from 'react'
 import { useForm, Controller } from "react-hook-form";
@@ -7,16 +7,19 @@ const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false }
 import "easymde/dist/easymde.min.css"
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import {z} from 'zod'
+import ErrorMessage from '@/app/components/ErrorMessage';
 
-interface IssueForm {
-  title: string,
-  description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 
 const NewIssuePage = () => {
   const router = useRouter()
-  const { register, control, handleSubmit } = useForm<IssueForm>()
+  const { register, control, handleSubmit, formState:{errors} } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  })
   const [error, setError] = useState('')
   return (
     <div className='max-w-xl'>
@@ -34,13 +37,14 @@ const NewIssuePage = () => {
           }
         })}>
         <TextField.Root placeholder="Issue"{...register('title')} />
+       <ErrorMessage>{errors.title?.message}</ErrorMessage >  
         <Controller
           name='description'
           control={control}
           render={({ field }) => <SimpleMDE placeholder="Description" {...field} />} />
+         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-
-        <button className='bg-violet-500 px-2.5 py-1.5 font-medium text-sm text-white rounded-lg cursor-pointer'>
+        <button className='bg-violet-500 px-2.5 py-1.5 font-medium text-sm  text-white rounded-lg cursor-pointer'>
           Submit New Issue
         </button>
       </form>
